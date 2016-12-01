66b560b73eb825b1b65ffc431e9a245c2d505e46
Michael Grein <michaelgrein92@gmail.com>
2016-12-01 00:04:05 -0500

    First Submission

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'pg'
 
 get '/' do
+  database = PG.connect(dbname: "tiy-sports")
+  @teams = database.exec("SELECT * FROM teams")
 
+  @memberships = database.exec("SELECT memberships.team_id FROM memberships")
   erb :home
 end
