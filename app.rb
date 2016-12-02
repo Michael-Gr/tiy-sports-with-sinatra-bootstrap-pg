134caada9392bb1b40af92b08dd99b3e1caf146a
Michael Grein <michaelgrein92@gmail.com>
2016-12-02 14:27:42 -0500

    dynamic number of players implemented

--- app.rb
+++ app.rb
@@ -3,8 +3,12 @@ require 'pg'
 
 get '/' do
   database = PG.connect(dbname: "tiy-sports")
-  @teams = database.exec("SELECT * FROM teams")
-
-  @memberships = database.exec("SELECT memberships.team_id FROM memberships")
+  @teams = database.exec("
+    SELECT    count(players.id), teams.name, teams.description,   teams.image_url
+    FROM      players, teams, memberships
+    WHERE     memberships.team_id = teams.id
+    AND       memberships.player_id = players.id
+    GROUP BY  teams.id, teams.name, teams.description, teams.image_url
+    ")
   erb :home
 end
